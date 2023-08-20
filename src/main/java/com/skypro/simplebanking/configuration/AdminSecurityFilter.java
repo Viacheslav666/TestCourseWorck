e346414cbb6f0ec3f1ec82511c53159b104c224f
Viacheslav <119102560+Viacheslav666@users.noreply.github.com>
2023-08-20 21:30:40 +0400

    First commit.

--- src/main/java/com/skypro/simplebanking/configuration/AdminSecurityFilter.java
+++ src/main/java/com/skypro/simplebanking/configuration/AdminSecurityFilter.java
@@ -1,12 +1,14 @@
 package com.skypro.simplebanking.configuration;
 
 import com.skypro.simplebanking.dto.BankingUserDetails;
+
 import java.io.IOException;
 import java.util.Optional;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,39 +20,41 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 public class AdminSecurityFilter extends OncePerRequestFilter {
-  private final String adminToken;
+    private final String adminToken;
 
-  public AdminSecurityFilter(@Value("${app.security.admin-token}") String adminToken) {
-    this.adminToken = adminToken;
-  }
+    public AdminSecurityFilter(@Value("${app.security.admin-token}") String adminToken) {
+        this.adminToken = adminToken;
+    }
+    //Данные аутентифицированного пользователя
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
 
-  @Override
-  protected void doFilterInternal(
-      @NonNull HttpServletRequest request,
-      @NonNull HttpServletResponse response,
-      @NonNull FilterChain filterChain)
-      throws ServletException, IOException {
-    Optional<UsernamePasswordAuthenticationToken> authenticatedUserDetails =
-        authenticateByKeyHeader(request);
-    authenticatedUserDetails.ifPresent(
-        details -> {
-          SecurityContext context = SecurityContextHolder.createEmptyContext();
-          context.setAuthentication(details);
-          SecurityContextHolder.setContext(context);
-        });
-    filterChain.doFilter(request, response);
-  }
+            throws ServletException, IOException {
+        Optional<UsernamePasswordAuthenticationToken> authenticatedUserDetails =
+                authenticateByKeyHeader(request);
+        authenticatedUserDetails.ifPresent(
+                details -> {
+                    SecurityContext context = SecurityContextHolder.createEmptyContext();
+                    context.setAuthentication(details);
+                    SecurityContextHolder.setContext(context);
+                });
+        filterChain.doFilter(request, response);
+    }
 
-  private Optional<UsernamePasswordAuthenticationToken> authenticateByKeyHeader(
-      HttpServletRequest request) {
-    return Optional.ofNullable(request.getHeader("X-SECURITY-ADMIN-KEY"))
-        .filter(StringUtils::hasText)
-        .filter(token -> token.contentEquals(adminToken))
-        .map(
-            stringKey -> {
-              BankingUserDetails userDetails = new BankingUserDetails(-1, "admin", "****", true);
-              return UsernamePasswordAuthenticationToken.authenticated(
-                  userDetails, "admin", userDetails.getAuthorities());
-            });
-  }
+    //аутентифицировать по ключевому заголовку
+    private Optional<UsernamePasswordAuthenticationToken> authenticateByKeyHeader(
+            HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader("X-SECURITY-ADMIN-KEY"))
+                .filter(StringUtils::hasText)
+                .filter(token -> token.contentEquals(adminToken))
+                .map(
+                        stringKey -> {
+                            BankingUserDetails userDetails = new BankingUserDetails(-1, "admin", "****", true);
+                            return UsernamePasswordAuthenticationToken.authenticated(
+                                    userDetails, "admin", userDetails.getAuthorities());
+                        });
+    }
 }
--- src/test/java/com/skypro/simplebanking/SimpleBankingApplicationTests.java
+++ src/test/java/com/skypro/simplebanking/SimpleBankingApplicationTests.java
@@ -1,9 +1,23 @@
 package com.skypro.simplebanking;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class SimpleBankingApplicationTests {
+    @Autowired
+    private DataSource dataSource;
+    @Test
+    void contextLoads() {
+    }
+
 
 }
